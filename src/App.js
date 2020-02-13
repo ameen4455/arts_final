b571c4897baf6f2b9c13235071807ea60b302195
ameen4455 <ameen4455@gmail.com>
2020-02-13 09:36:50 +0530

    final

--- src/App.js
+++ src/App.js
@@ -14,12 +14,22 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
+      border:true,
       styl:false,
       samhara : 0,
       rakshasa : 0,
       dhruva : 0,
       thandava : 0
     }
+  }
+
+  changeBorderClass(){
+    this.setState({border:false},()=>
+        {
+          setTimeout(() => {this.setState({border : true})}, 1500
+          )
+        }
+    )
   }
 
   componentDidMount() {
@@ -34,33 +44,39 @@ class App extends React.Component {
         alert(error);
       });
       setTimeout(() => {
-        const {thandava, dhruva, rakshasa, samhara} = score.chakravyuh;
+        const {thandava, dhruva, rakshasa, samhara ,topper} = score.chakravyuh;
         this.setState({
+          type : "Chakravyuh",
           samhara : samhara,
           rakshasa : rakshasa,
           dhruva : dhruva,
-          thandava : thandava
-        })
+          thandava : thandava,
+        }, () => this.changeBorderClass() );
+        setTimeout(()=> this.setState({ topper : topper}) ,1500) ;
         setTimeout(() => {
-          const {thandava, dhruva, rakshasa, samhara} = score.layatharang;
+          const {thandava, dhruva, rakshasa, samhara ,topper} = score.layatharang;
           this.setState({
+            type:"Laytharang",
             samhara : samhara,
             rakshasa : rakshasa,
             dhruva : dhruva,
-            thandava : thandava
-          })
+            thandava : thandava,
+          } , () => this.changeBorderClass() );
+          setTimeout(()=> this.setState({ topper : topper}) ,1500) ;
           setTimeout(() => {
-            const {thandava, dhruva, rakshasa, samhara} = score.overall_score;
+            const {thandava, dhruva, rakshasa, samhara ,topper} = score.overall_score;
             this.setState({
+              type:"Overall",
               samhara : samhara,
               rakshasa : rakshasa,
               dhruva : dhruva,
-              thandava : thandava
-            })
+              thandava : thandava,
+            } , () => this.changeBorderClass() );
+            setTimeout(()=> this.setState({ topper : topper}) ,1500) ;
           }, 30000);
         }, 30000);
       }, 30000);
-    }, 30000);
+    }, 90000);
     setTimeout(() => this.setState({styl:true}),3000)
   }
 
@@ -72,9 +88,13 @@ class App extends React.Component {
           <div className="App" style={ this.state.styl ? {opacity:"1"}:null}>
           <div className="mainBox">
             <div className="title">
-              Layatharang
+              {this.state.type}
             </div>
-            <div className="scoreBox">
+            <div className={ this.state.border ? "scoreBox" : "scoreBoxChange"}
+                 style={this.state.topper === "rakshasa" ?  {borderColor:"#1976d2"} :
+                     this.state.topper === "thandava" ?  {borderColor:"#d32f2f"} :
+                         this.state.topper === "dhruva" ?  {borderColor:"#f57c00"} : {borderColor:"#6a1b9a"}
+                 } >
               <Card hname={Samhara} hcolor="#6a1b9a" hpoint={this.state.samhara} />
               <Card hname={Rakshasa} hcolor="#1976d2" hpoint={this.state.rakshasa} />
               <Card hname={Druva} hcolor="#f57c00" hpoint={this.state.dhruva} />
